91cf0ac2f222f96fc8643b6001fad0d041acb745
LalkaBel <Belikova_nn@mail.ru>
2021-04-24 20:26:14 +0300

    Signed-off-by: LalkaBel <Belikova_nn@mail.ru>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <QCoreApplication>
-
+#include<iostream>
+using namespace std;
+using namespace Qt;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    int k=5;
+    int j=10;
+    int v,R;
+    v=k+j;
+    R=k*j;
+    cout<<v;
+    cout<<R;
     return a.exec();
 }
